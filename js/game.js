107f3915b7b8c30b8049a360fbf96b37ee7ac4c4
Brayden Blank <99354754+Brayden-Blank@users.noreply.github.com>
2022-06-08 17:16:26 +0000

    revert back to lucas version

--- js/game.js
+++ js/game.js
@@ -1,21 +1,12 @@
 /* global Phaser */
 
-//Copyright (c) 2022 Brayden Blank All rights reserved
-//
-// Created by: Brayden Blank
+// Created by: Lucas
 // Created on: April 2022
 // This is the Phaser3 game configuration file
 
-// scene import statements
-import SplashScene from "./splashScene.js"
-
-//create the new scenes
-const splashScene = new SplashScene()
-
 /**
-* Start Phaser Game.
-*/
-
+ * Start Phaser Game.
+ */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
@@ -36,11 +27,4 @@ const config = {
 }
 
 const game = new Phaser.Game(config)
-// console.log(game)
-
-// load scenes
-// Note: remember any "key" is global and CAN NOT be reused!
-game.scene.add("splashScene", splashScene)
-
-// the start scene
-game.scene.start("splashScene")
+console.log(game)
