3e231cd868ae274c78719dbb2e19ced80e2b4294
Brayden Blank <99354754+Brayden-Blank@users.noreply.github.com>
2022-06-14 16:23:33 +0000

    scenes

--- js/menuScene.js
+++ js/menuScene.js
@@ -46,7 +46,7 @@ class MenuScene extends Phaser.Scene {
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   create(data) {
-    this.menuSceneBackgroundImage = this.add.sprite(0, 0, "menuSceneBackground")
+    this.menuSceneBackgroundImage = this.add.sprite(0, 0, "menuSceneBackground").setScale(2)
     this.menuSceneBackgroundImage.x = 1920 / 2
     this.menuSceneBackgroundImage.y = 1080 / 2
 
