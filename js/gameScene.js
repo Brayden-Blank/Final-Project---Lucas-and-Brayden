d9c535ee3a40959ff286999f8769cece65d45ab5
Lucas Tyman <99354994+Lucas-Tyman@users.noreply.github.com>
2022-06-16 16:30:45 +0000

    fixed

--- js/gameScene.js
+++ js/gameScene.js
@@ -88,11 +88,12 @@ class GameScene extends Phaser.Scene {
         asteroidCollide.destroy()
         millenniumFalconCollide.destroy()
         this.gameOverText = this.add
-          .text(1920 / 2,
-               1080 / 2,
-               "Game Over!\nClick to play again.",
-               this.gameOverTextStyle
-              )
+          .text(
+            1920 / 2,
+            1080 / 2,
+            "Game Over!\nClick to play again.",
+            this.gameOverTextStyle
+         )
           .setOrigin(0.5)
         this.gameOverText.setInteractive({ useHandCursor: true })
         this.score = 0
